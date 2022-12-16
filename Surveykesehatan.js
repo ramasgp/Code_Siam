d7cc4f8992beb215e9b58765ee7b679297d7e6b0
Rama Sakti Gagah Prawiratama <107415776+ramasgp@users.noreply.github.com>
2022-12-16 07:22:19 +0700

    Update Surveykesehatan.js

--- Surveykesehatan.js
+++ Surveykesehatan.js
@@ -1,1 +1,13 @@
+var inputElements = document.getElementsByTagName('input');
 
+for(let inputElement of inputElements) {
+  if(inputElement.value == "Tidak" && inputElement.name == "rad_konfirmasi") {
+    inputElement.checked = true;
+  }else if(inputElement.value == "Sehat" && inputElement.name == "rad_kondisi") {
+    inputElement.checked = true;
+  }else if(inputElement.value == "Tidak" && inputElement.name == "rad_kontak") {
+    inputElement.checked = true;
+  }
+}
+
+jQuery("#form_survey").submit();
